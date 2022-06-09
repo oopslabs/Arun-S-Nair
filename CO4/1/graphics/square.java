eef198a9afb0750a3b1b6a9a23d7a13587513bb9
Arun S Nair <93431805+arunsnair135@users.noreply.github.com>
2022-06-09 11:39:27 +0530

    Add files via upload

---
+++ CO4/1/graphics/square.java
@@ -0,0 +1,12 @@
+package graphics;
+
+public class square {
+	int a;
+	public square(int i){
+		a=i;
+	}
+	
+	public void area(){
+		System.out.println(a*a);
+	}
+}
---
+++ CO4/1/graphics/triangle.java
@@ -0,0 +1,15 @@
+package graphics;
+
+public class triangle {
+	int b,h;
+	public triangle(int i,int j){
+		h=j;
+		b=i;
+	}
+	
+	public void area(){
+		System.out.println(0.5*b*h);
+	}
+	
+	
+}
